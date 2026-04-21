217b5a7e48e47e93d56d7377341abf37327b5fba
ojasarora77 <ojas4jan@gmail.com>
2026-04-21 15:35:49 +0100

    fix: support for sol ver 8.34 overall

--- test/solidity/MorphoBlueFuzz.t.sol
+++ test/solidity/MorphoBlueFuzz.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.28;
+pragma solidity ^0.8.34;
 
 import { Id, MarketParams } from "@morpho-org/morpho-blue/src/interfaces/IMorpho.sol";
 import { MarketParamsLib } from "@morpho-org/morpho-blue/src/libraries/MarketParamsLib.sol";
